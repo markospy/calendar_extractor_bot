70a8b0ef5ca08fe53a00b77eacee5bcd03a315f4
Marcos Avila <mis.redes.work@gmail.com>
2023-10-27 21:03:29 -0400

    Se eliminó la llamada a una función inexistente.

--- run.py
+++ run.py
@@ -3,6 +3,5 @@ from calendario.interfaz import Interfaz, scraping
 if __name__ == "__main__":
     program = Interfaz()
     program.menu()
-    program.record_config()
     scraping(program.path)
     program.programar_rapado()
